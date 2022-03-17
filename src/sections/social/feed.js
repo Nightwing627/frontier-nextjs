edbc12f1dcc6d7979835005f2bdd3e6fcc572d49
TMD <webdev0627@gmail.com>
2022-03-17 09:33:51 -0400

    processing the social feed page

--- src/sections/social/feed.js
+++ src/sections/social/feed.js
@@ -13,7 +13,11 @@ import {
   Card,
 } from "theme-ui"
 import React, { useState } from "react"
+import { FaGlobe } from 'react-icons/fa'
 import Avatar1 from 'assets/user-avatars/avatar.png'
+import Happy from 'assets/social/happy.svg'
+import ImageF from 'assets/social/image.svg'
+import Doc from 'assets/social/document.svg'
 
 export default function Feed() {
   return (
@@ -24,15 +28,25 @@ export default function Feed() {
             <Card sx={styles.mind}>
               <Flex>
                 <Image src={Avatar1} sx={styles.mind.avatar}/>
-                <Box sx={{ ml: 3}}>
-                  <Flex sx={{ alignItem: 'center', justifyContent: 'space-between' }}>
-                    <Text as="span">What's on you mind, lqbal?</Text>
+                <Box sx={{ pl: '30px', width: '100%' }}>
+                  <Flex sx={ styles.mind.part1 }>
+                    <Text as="span" sx={{ fontSize: '20px', color: '#203758' }}>
+                      What's on you mind, lqbal?</Text>
                     <Button sx={styles.mind.post}>Post</Button>
                   </Flex>
-                  
+                  <Flex sx={styles.mind.part2}>
+                    <FaGlobe sx={{ fontSize: '20px' }}/>
+                    <Text as="p" sx={styles.mind.reply}>Everyone can reply</Text>
+                  </Flex>
+                  <Flex sx={{ alignItems: 'center', pt: '15px' }}>
+                    <Image src={ImageF} sx={styles.mind.svgIcon} />
+                    <Image src={Doc} sx={styles.mind.svgIcon} />
+                    <Image src={Happy} sx={styles.mind.svgIcon} />
+                  </Flex>
                 </Box>
               </Flex>
             </Card>
+            
             <Card>
               
             </Card>
@@ -84,13 +98,36 @@ const styles = {
     borderRadius: '10px',
     p: 5,
     avatar: {
+      width: '60px',
+      height: '60px',
+      minWidth: 'auto',
       borderRadius: '50%',
+    },
+    part1: {
+      position: 'relative',
+      alignItems: 'center',
+      borderBottom: '1px solid rgba(0, 0, 0, 0.1)',
+      justifyContent: 'space-between',
+      pb: '10px',
     },
     post: {
       fontSize: '18px',
       fontFamily: 'mazzard-h-bold',
       color: 'white',
       bg: 'button_color',
+    },
+    part2: {
+      alignItems: 'center',
+      py: '15px',
+      borderBottom: '1px solid rgba(0, 0, 0, 0.1)',
+    },
+    reply: {
+      fontSize: '15px',
+      fontFamily: 'mazzard-h-bold',
+      ml: '10px',
+    },
+    svgIcon: {
+      mr: '15px',
     }
   }
 }
