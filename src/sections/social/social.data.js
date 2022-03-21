a56fefda08d106f2d931f49610e793fc63dbd80b
TMD <webdev0627@gmail.com>
2022-03-21 05:18:03 -0400

    complete the social notification page

--- src/sections/social/social.data.js
+++ src/sections/social/social.data.js
@@ -6,6 +6,7 @@ import storyImg3 from 'assets/social/story/3.jpg'
 import avatar1 from 'assets/user-avatars/avatar_5.png' 
 import avatar2 from 'assets/user-avatars/avatar_6.png' 
 import avatar3 from 'assets/user-avatars/avatar_7.png' 
+import avatar4 from 'assets/user-avatars/avatar_8.png' 
 
 import { FaHeart } from 'react-icons/fa'
 
@@ -99,4 +100,66 @@ export const FilterData = [
     { id: 4, name: 'Mentions',  status: 'checked' },
     { id: 5, name: 'Purchases', status: '' },
     { id: 6, name: 'Message',   status: '' },
+]
+
+export const NotiDetail = [
+    {
+        id: 1,
+        avatar: avatar4,
+        title: {
+            target: 'Miranda Shaffer',
+            bridge: 'added a photo in',
+            from: 'Design Group'
+        },
+        description: '',
+        time: 6
+    },
+    {
+        id: 2,
+        avatar: avatar4,
+        title: {
+            target: 'Elon Musk',
+            bridge: 'liked a photo from',
+            from: 'Neil deGrasse Tyson'
+        },
+        description: `Congratulations to @elonmusk and his Falcon 9 rockets for helping @NASA & @ESA send space probes around the solar system.</br>
+            Tonight, the DART (Double Asteroid Redirect Test) misssion launches to deflect a potentially hazardous asteroid. https://pic.twitter.com/SmJkS1QFgi`,
+        time: 7
+    },
+    {
+        id: 3,
+        avatar: avatar4,
+        title: null,
+        description: `<b>Iqbal Hossain</b> There was a login to your account @Iqbal01717 from a new device on 
+        Nov 20, 2021. Review it now.`,
+        time: 8
+    },
+    {
+        id: 4,
+        avatar: avatar4,
+        title: {
+            target: 'Elon Musk',
+            bridge: 'liked a photo from',
+            from: 'Neil deGrasse Tyson'
+        },
+        description: `Congratulations to @elonmusk and his Falcon 9 rockets for helping @NASA & @ESA send space probes around the solar system.</br>
+            Tonight, the DART (Double Asteroid Redirect Test) misssion launches to deflect a potentially hazardous asteroid. https://pic.twitter.com/SmJkS1QFgi`,
+        time: 5
+    },
+    {
+        id: 5,
+        avatar: avatar4,
+        title: null,
+        description: `<b>Iqbal Hossain</b> There was a login to your account @Iqbal01717 from a new device on 
+        Nov 20, 2021. Review it now.`,
+        time: 6
+    },
+    {
+        id: 6,
+        avatar: avatar4,
+        title: null,
+        description: `<b>Iqbal Hossain</b> There was a login to your account @Iqbal01717 from a new device on 
+        Nov 20, 2021. Review it now.`,
+        time: 6
+    },
 ]
